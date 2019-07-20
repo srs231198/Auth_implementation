5921aba3dbff4c49db656e6a025e04bf2330b092
Shreyas Raj Singh <srs231198@gmail.com>
2019-07-20 09:40:46 +0530

    setUp2
    Set up for MongoDB user schema and passport.js is done!

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,9 +1,28 @@
-var express     = require("express"),
-    app         = express()
-    // bodyParser  = require("body-parser"),
-    // mongoose    = require("mongoose")
+var express               = require("express"),
+    app                   = express(),
+    bodyParser            = require("body-parser"),
+    mongoose              = require("mongoose"),
+    User                  = require("./models/user"),
+    passport              = require("passport"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose")
 
+//APP CONFIG
+mongoose.connect("mongodb://localhost/auth_demo_app", { useNewUrlParser: true });
+
+app.use(require("express-session")({
+    secret: "what is 2 + 2?",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //INDEX ROUTE
 app.get("/", function(req, res) {
