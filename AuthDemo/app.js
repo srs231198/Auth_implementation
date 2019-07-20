7378d21feefe5fee9fb8bb3d4a9938c452d60188
Shreyas Raj Singh <srs231198@gmail.com>
2019-07-20 10:18:47 +0530

    LoginRouteSetUp
    The Login Routes have been set up!

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -18,9 +18,10 @@ app.use(require("express-session")({
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
 app.use(passport.initialize());
 app.use(passport.session());
-
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -54,6 +55,20 @@ app.post("/register", function(req, res) {
         });
     });
 });
+
+//LOGIN ROUTES
+//render login forms
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+//login logic
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}) ,function(req, res){
+
+});
+
 
 app.listen(3000, function(){
     console.log("Server is running");
